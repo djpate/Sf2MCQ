c4f7db476c6efa5d1e64d5868bf1a7e155e43f10
Christophe Verbinnen <djpate@gmail.com>
2011-06-08 17:01:57 +0200

    this is bugged !

--- src/Sf2MCQ/CoreBundle/Model/TestModel.php
+++ src/Sf2MCQ/CoreBundle/Model/TestModel.php
@@ -20,8 +20,23 @@ namespace Sf2MCQ\CoreBundle\Model;
 			return ($start->format("U") + (60 * $this->getInterview()->getDuration())) - time();
 		}
 		
-		public function isAnswered(Sf2MCQ\CoreBundle\Entity\Question $q){
+		public function isAnswered(\Sf2MCQ\CoreBundle\Entity\Question $q){
 			
+		}
+		
+		public function isSelected(\Sf2MCQ\CoreBundle\Entity\Question $q,\Sf2MCQ\CoreBundle\Entity\Answer $a){
+			$conn = $this->get('database_connection');
+			$sql = 'SELECT answer_id FROM `proposition`,`proposition_answers` where test_id = ? and question_id = ? and answer_id = ? and proposition.id = proposition_answers.proposition_id';
+			$conn->prepare($sql);
+			$stmt->bindValue(1, $this->getId() );
+			$stmt->bindValue(2, $q->getId() );
+			$stmt->bindValue(3, $a->getId() );
+			$res = $stmt->execute();
+			if($res==1){
+				return true;
+			} else {
+				return false;
+			}
 		}
 	
 	}
